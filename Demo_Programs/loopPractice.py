33eab61685b4b7c29e5966f3d8ed94c5389979fe
Edwin Kim <edwin.kim@GCCYJPVMJ1WK.local>
2020-10-02 13:23:02 -0400

    push

---
+++ Demo_Programs/loopPractice.py
@@ -0,0 +1,16 @@
+sum = 0
+
+for i in range(0,101,2):
+	sum = sum + i
+
+print(sum)    
+
+
+data = [100,56,90,32,54,12,43,65,234,654,42]
+ 
+sum = 0
+for i in range(0, len(data), 1):
+	sum = sum + data[i]
+
+average = sum/len(data)
+print(average)
