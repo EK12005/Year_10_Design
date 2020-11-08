c379cb365b65efc3f438bdadedd1cbfd64127aea
Edwin Kim <edwin.kim@GCCYJPVMJ1WK.local>
2020-11-08 17:15:10 -0500

    push

--- Demo_Programs/loopPractice.py
+++ Demo_Programs/loopPractice.py
@@ -1,16 +1,20 @@
-sum = 0
+
+'''
+sum = 0 
 
 for i in range(0,101,2):
 	sum = sum + i
 
 print(sum)    
+'''
 
-
-data = [100,56,90,32,54,12,43,65,234,654,42]
+data = [100,65,90,32,54,12,43,65,234,654,42] 
  
 sum = 0
 for i in range(0, len(data), 1):
 	sum = sum + data[i]
+
+print(sum)
 
 average = sum/len(data)
 print(average)
--- Ferengi_Currency_Relocation_Product/accessAPI.py
+++ Ferengi_Currency_Relocation_Product/accessAPI.py
@@ -6,12 +6,12 @@ from pprint import pprint
 
 #Get Key
 #This is a file not in my respository I don't want you to have it
-file = open("..//..//API_Keys//fixerkey.txt","r")
+file = open("..//..//API_Keys//fixerkey.txt","r") #Gets access key
 
 key = file.read()
 
 
-resp = requests.get('http://data.fixer.io/api/latest?access_key='+key)
+resp = requests.get('http://data.fixer.io/api/latest?access_key='+key) #gets the API data
 #resp = requests.get('https://jsonplaceholder.typicode.com/comments')
 
 
