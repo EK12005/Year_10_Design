0756ad3b451bce9ecc91165ca3738d834af20db2
Edwin Kim <edwin.kim@GCCYJPVMJ1WK.local>
2021-05-13 23:02:54 -0400

    data structures update

--- Contest_Problems/CCC/CCC_J2_2021.py
+++ Contest_Problems/CCC/CCC_J2_2021.py
@@ -1,3 +1,5 @@
+'''
+#Approach 1 (List)
 n = int(input())
 
 data = []
@@ -11,8 +13,6 @@ while i < n:
 	data.append(value)
 	i = i + 1
 
-print(data)
-
 largest = data[1]
 
 largestindex = 1
@@ -24,3 +24,33 @@ for c in range(1,len(data),2):
 		largestindex = c
 
 print(data[largestindex - 1])
+'''
+
+#Approach 2 (Dictionary)
+n = int(input())
+
+data = {
+	"bidname": [],
+	"bidvalue": []
+}
+
+i = 0
+
+while i < n:
+	name = input()
+	value = int(input())
+	data["bidname"].append(name)
+	data["bidvalue"].append(value)
+	i = i + 1
+
+largest = data["bidvalue"][0]
+
+largestindex = 0
+
+for c in range(1,len(data["bidvalue"]),1):
+
+	if largest < data["bidvalue"][c]:
+		largest = data["bidvalue"][c]
+		largestindex = c
+
+print(data["bidname"][largestindex])
--- Project3/SumList.py
+++ Project3/SumList.py
@@ -1,13 +1,16 @@
+'''
 #Approach 1: Finding The Sum Of A Set List Using Recursion
 def sumList1(intList):
 
 	if len(intList) == 1:
 		return intList[0]
 	else:
-		return intList[0] + sumList(intList[1:])
+		return intList[0] + sumList1(intList[1:])
 
-intList = [1,2,3,4,5]
+intList = [3,2,4,4,6]
 print(sumList1(intList))
+
+'''
 
 #Approach 2: Finding The Sum Of A Set List Using Iteration
 def sumList2(intList):
@@ -18,6 +21,6 @@ def sumList2(intList):
 		total += intList[i]
 	return total
 
-intList = [1,2,3,4,5]
+intList = [3,2,4,4,6]
 print(sumList2(intList))
 
